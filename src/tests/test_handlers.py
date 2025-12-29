889161944f0a67e610f4cf09ba470aeeab8bdc2e
Gabriel C. Ullmann <ullmanng162@gmail.com>
2025-12-29 10:32:38 -0500

    add handler tests

--- src/tests/test_handlers.py
+++ src/tests/test_handlers.py
@@ -4,6 +4,7 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 from datetime import datetime
+from os.path import exists
 from handlers.user_created_handler import UserCreatedHandler
 from handlers.user_deleted_handler import UserDeletedHandler
 
@@ -17,13 +18,17 @@ def test_user_created_handler():
         "datetime": str(datetime.now())
     }
     user_creation.handle(mock_event)
+    file_exists = exists("output/welcome_998.html")
+    assert file_exists == True
 
 def test_user_deleted_handler():
-    user_creation = UserDeletedHandler()
+    user_deletion = UserDeletedHandler()
     mock_event = {
         "id": 999,
         "name": 'Joe Test',
         "email": 'joetest@example.com',
         "datetime": str(datetime.now())
     }
-    user_creation.handle(mock_event)
+    user_deletion.handle(mock_event)
+    file_exists = exists("output/welcome_999.html")
+    assert file_exists == True
